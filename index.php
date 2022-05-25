76a0a0801c92fdf2484b4f412f72ada3f65e8dca
ArevaloJoshua <joshua.arevalo435@gmail.com>
2022-05-25 19:03:59 +0800

    index has been added

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>MovieZoneWeb</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+
+  <section class="movie">
+      <nav>
+        <div>
+          <b>
+          <span style="color: white; font-size: 60px;">What</span>
+          <span style="color: hotpink; font-size: 60px;">The?</span>
+        </b>
+        </div>
+
+        <div>
+          <input type="text" placeholder="Searching..." style="color: white; width: 60%; height: 50px; padding: 0px;  font-size: 20px; background-color: transparent;">
+        </div>
+
+        <div>
+          <br>
+        <img src="assets/background.jpg" style="width: 100%; height: 400px;">
+          </br>
+        </div>
+        
+      </nav>
+    </section>
+
+<style>.profile-head {
+    transform: translateY(5rem)
+}
+body {
+    background: black;
+    min-height: 100vh;
+    overflow-x: hidden
+</style>
+
+<div class="py-4 px-4">
+<div class="topnav">
+  <h2>
+    <ul>
+      <li><a href="index.php" style="background-color:black; color: hotpink;">Home</a></li>
+      <li><a href="page1.php">Recommended</a></li>
+      <li><a href="page2.php">Action</a></li>
+      <li><a href="page3.php">Horror</a></li>
+      <li><a href="page4.php">Romance</a></li>
+      <li><a href="page5.php">Fantasy</a></li>
+      <li><a href="page6.php">Comedy</a></li>
+      <li><a href="page7.php">K-Drama</a></li>
+      <li><a href="page8.php">Adventure</a></li>
+      <li><a href="page9.php">Animation</a></li>
+      <li><a href="page10.php">Tagalog</a></li>
+      <li><a href="page11.php">Family</a></li>
+      <li><a href="page12.php">Superhero</a></li>
+</ul>
+</h2>
+</div>
+</div>
+
+<style>
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 10;
+  overflow: hidden;
+  background-color: hotpink;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 21px;
+  text-decoration: none;
+  font-family: ;
+}
+
+li a:hover {
+  background-color: lightpink;
+}
+</style>
+
+
+<div class="py-4 px-4">
+  <img src="reco/rec3.jpg" style="width: 12%; height: 300px;">
+  <img src="action/act5.jpg" style="width: 12%; height: 300px;">
+  <img src="comedy/cm3.jpg" style="width: 12%; height: 300px;">
+  <img src="horror/hor4.jpg" style="width: 12%; height: 300px;">
+  <img src="romance/rom5.jpg" style="width: 12%; height: 300px;">
+  <img src="reco/rec5.jpg" style="width: 12%; height: 300px;">
+  <img src="comedy/cm5.jpg" style="width: 12%; height: 300px;">
+  <img src="horror/hor3.jpg" style="width: 12%; height: 300px;">
+</div>
+</div>
+
+<div class="px-4 py-3">
+                <div class="p-4 rounded shadow-sm bg-light">
+                    <p style="color: white; font-size: 20px; text-align: center;">All Rights Reserved. Copyright 2022</p>
+                </div>
+            </div>
+                             
+
+</body>
+</html>
