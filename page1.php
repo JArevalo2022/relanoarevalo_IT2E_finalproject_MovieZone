3c2bf2eb15711557a217c4e41e8964bb690bcaaa
ArevaloJoshua <joshua.arevalo435@gmail.com>
2022-05-27 21:09:31 +0800

    page 1 added

---
+++ page1.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>MovieZoneWeb</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+  
+
+  <section class="movie">
+      <nav>
+        <div>
+          <b>
+          <span style="color: white; font-size: 60px;">Movie</span>
+          <span style="color: hotpink; font-size: 60px;">Zone</span>
+        </b>
+        </div>
+
+        <div>
+          <input type="text" placeholder="Searching..." style="color: white; width: 60%; height: 50px; padding: 0px;  font-size: 20px; background-color: transparent;">
+        </div>
+      </nav>
+    </section>
+
+<style>.profile-head {
+    transform: translateY(5rem)
+}
+body {
+    background: black;
+    min-height: 100vh;
+    overflow-x: hidden
+</style>
+
+<div class="py-4 px-4">
+<div class="topnav">
+  <h2>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="page1.php" style="background-color:black; color: hotpink;">Recommended</a></li>
+      <li><a href="page2.php">Action</a></li>
+      <li><a href="page3.php">Horror</a></li>
+      <li><a href="page4.php">Romance</a></li>
+      <li><a href="page5.php">Fantasy</a></li>
+      <li><a href="page6.php">Comedy</a></li>
+      <li><a href="page7.php">K-Drama</a></li>
+      <li><a href="page8.php">Adventure</a></li>
+      <li><a href="page9.php">Animation</a></li>
+      <li><a href="page10.php">Tagalog</a></li>
+      <li><a href="page11.php">Family</a></li>
+      <li><a href="page12.php">Superhero</a></li>
+</ul>
+</h2>
+</div>
+</div>
+
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 10;
+  overflow: hidden;
+  background-color: hotpink;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 21px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: lightpink;
+}
+</style>
+
+<div class="py-4 px-4">
+<div class="topnav">
+
+  <img src="reco/rec1.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> Sucker Punch </a>
+</div>
+
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;"> Your opinion on Sucker Punch will likely hinge on your opinion of Zack Snyder’s particular style.This steampunk fantasy delves into the imagination of Babydoll (Emily Browning), a young woman locked in an asylum who wages a war for freedom inside her mind alongside four other women. Together, and with an absolute arsenal at their disposal, they take on everything from samurais to serpents and gamble it all on survival.</p> 
+</div>     
+
+<div>
+  <img src="reco/rec2.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> Big Fish </a>
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;"> Tim Burton’s folksy fantasy is one that many of us can identify with. It’s the manifestation of an old man’s hyperbolic stories of his life. Edward Bloom (Albert Finney) has always told seemingly tall tales about his life as fact, a trait that has made his relationship with his son, William (Billy Crudup), difficult. But when Edward continues to tell these fantasy stories on his deathbed, William decides to finally investigate his father’s tales. As he does, he begins to finally understand his father after a lifetime of confusion.</p>
+    </div>
+
+<div>
+  <img src="reco/rec3.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  < <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> Green Mile</a>
+ 
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;"> The lives of guards on Death Row are affected by one of their charges: a black man accused of child murder and rape, yet who has a mysterious gift. Death Row guards at a penitentiary, in the 1930's, have a moral dilemma with their job when they discover one of their prisoners, a convicted murderer, has a special gift.</p>
+    </div>
+
+<div>
+  <img src="reco/rec4.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  < <a href="url"  style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> How To Train Your Dragon</a>
+
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;">Long ago up North on the Island of Berk, the young Viking, Hiccup, wants to join his town's fight against the dragons that continually raid their town. However, his macho father and village leader, Stoik the Vast, will not allow his small, clumsy, but inventive son to do so. Regardless, Hiccup ventures out into battle and downs a mysterious Night Fury dragon with his invention, but can't bring himself to kill it. Instead, Hiccup and the dragon, whom he dubs Toothless, begin a friendship that would open up both their worlds as the observant boy learns that his people have misjudged the species. But even as the two each take flight in their own way, they find that they must fight the destructive ignorance plaguing their world. </p>
+    </div>
+
+<div>
+  <img src="reco/rec5.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  < <a href="url"  style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> Her </a>
+  
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;"> Theodore is a lonely man in the final stages of his divorce. When he's not working as a letter writer, his down time is spent playing video games and occasionally hanging out with friends. He decides to purchase the new OS1, which is advertised as the world's first artificially intelligent operating system, "It's not just an operating system, it's a consciousness," the ad states. Theodore quickly finds himself drawn in with Samantha, the voice behind his OS1. As they start spending time together they grow closer and closer and eventually find themselves in love.</p>
+    </div>
+ 
+ </body>
+ </html>
