7bcdb59ed140cbe254c961c176cede5a3811fd08
ArevaloJoshua <joshua.arevalo435@gmail.com>
2022-05-27 21:10:25 +0800

    page 2 added

---
+++ page2.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>MovieZoneWeb</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+  
+
+  <section class="movie">
+      <nav>
+        <div>
+          <b>
+          <span style="color: white; font-size: 60px;">Movie</span>
+          <span style="color: hotpink; font-size: 60px;">Zone</span>
+        </b>
+        </div>
+
+        <div>
+          <input type="text" placeholder="Searching..." style="color: white; width: 60%; height: 50px; padding: 0px;  font-size: 20px; background-color: transparent;">
+        </div>
+      </nav>
+    </section>
+
+<style>.profile-head {
+    transform: translateY(5rem)
+}
+body {
+    background: black;
+    min-height: 100vh;
+    overflow-x: hidden
+</style>
+
+<div class="py-4 px-4">
+<div class="topnav">
+  <h2>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="page1.php">Recommended</a></li>
+      <li><a href="page2.php" style="background-color:black; color: hotpink;">Action</a></li>
+      <li><a href="page3.php">Horror</a></li>
+      <li><a href="page4.php">Romance</a></li>
+      <li><a href="page5.php">Fantasy</a></li>
+      <li><a href="page6.php">Comedy</a></li>
+      <li><a href="page7.php">K-Drama</a></li>
+      <li><a href="page8.php">Adventure</a></li>
+      <li><a href="page9.php">Animation</a></li>
+      <li><a href="page10.php">Tagalog</a></li>
+      <li><a href="page11.php">Family</a></li>
+      <li><a href="page12.php">Superhero</a></li>
+</ul>
+</h2>
+</div>
+</div>
+
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 10;
+  overflow: hidden;
+  background-color: hotpink;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 21px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: lightpink;
+}
+</style>
+
+<div>
+  <img src="action/act1.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  < <a href="url"  style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> Saving Private Ryan</a>
+
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;"> After the invasion of fortress Europe on June 6th 1944, Cpt. Miller leads his squad from the 2nd Ranger Battalion of the 29th Infantry Division, on a mission to find and bring home Private James Francis Ryan after the death of his brothers. The mission takes them through Nazi occupied territory to establish contact with Ryan's unit, an element of the 101st Airborne Division. This exciting war thriller brings the reality of history's bloodiest war into the homes of ordinary people, but also brings to light the reality of broken and lost families in a time of total and encompassing war.</p>
+    </div>
+
+<div>
+  <img src="action/act2.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  < <a href="url"  style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> Casino Royale</a>
+  
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;"> Recently promoted to 00 status, James Bond (Daniel Craig) takes on his first mission, in which he faces a mysterious private banker to world terrorism and poker player, Le Chiffre (Mads Mikkelsen). Along with beautiful Treasury Agent Vesper Lynd (Eva Green) and the MI6 man in Montenegro, Bond takes part in a high stakes poker game set up by Le Chiffre in order to recover a huge sum of his clients' money he lost in a failed plot that the British spy took down. 007 will not only discover the threatening organization behind his enemy, but the worst of all truths: to not trust anyone.</p>
+    </div>
+
+<div>
+  <img src="action/act3.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url"  style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> Eagle Eye</a>
+  
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;">Jerry and Rachel are two strangers thrown together by a mysterious phone call from a woman they have never met. Threatening their lives and family, she pushes Jerry and Rachel into a series of increasingly dangerous situations, using the technology of everyday life to track and control their every move. </p>
+    </div>
+
+<div>
+  <img src="action/act4.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url"  style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> John Rambo</a>
+ 
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;">  He first appeared in the 1972 novel First Blood by David Morrell, but later became more famous as the protagonist of the film series, in which he was played by Sylvester Stallone. The portrayal of the character earned Stallone widespread acclaim and recognition. The character was nominated for American Film Institute's list 100 Years…100 Heroes and Villains. The term "Rambo" is used commonly to describe a lone wolf who is reckless, disregards orders, uses violence to solve all problems, enters dangerous situations alone, and is exceptionally tough, callous, raw and aggressive.</p>
+    </div>
+
+<div>
+  <img src="action/act5.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";>Desperado </a>
+
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;"> Former musician and gunslinger El Mariachi arrives at a small Mexican border town after being away for a long time. His past quickly catches up with him and he soon gets entangled with the local drug kingpin Bucho and his gang.</p>
+    </div>
+
+
+
+</body>
+</html>
