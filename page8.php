25046b7aba58225929fd3822203806d61f6f9a26
ArevaloJoshua <joshua.arevalo435@gmail.com>
2022-05-27 21:15:23 +0800

    page8 added

---
+++ page8.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>MovieZoneWeb</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+  
+
+  <section class="movie">
+      <nav>
+        <div>
+          <b>
+          <span style="color: white; font-size: 60px;">Movie</span>
+          <span style="color: hotpink; font-size: 60px;">Zone</span>
+        </b>
+        </div>
+
+        <div>
+          <input type="text" placeholder="Searching..." style="color: white; width: 60%; height: 50px; padding: 0px;  font-size: 20px; background-color: transparent;">
+        </div>
+      </nav>
+    </section>
+
+<style>.profile-head {
+    transform: translateY(5rem)
+}
+body {
+    background: black;
+    min-height: 100vh;
+    overflow-x: hidden
+</style>
+
+<div class="py-4 px-4">
+<div class="topnav">
+  <h2>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="page1.php">Recommended</a></li>
+      <li><a href="page2.php">Action</a></li>
+      <li><a href="page3.php">Horror</a></li>
+      <li><a href="page4.php">Romance</a></li>
+      <li><a href="page5.php">Fantasy</a></li>
+      <li><a href="page6.php">Comedy</a></li>
+      <li><a href="page7.php">K-Drama</a></li>
+      <li><a href="page8.php" style="background-color:black; color: hotpink;">Adventure</a></li>
+      <li><a href="page9.php">Animation</a></li>
+      <li><a href="page10.php">Tagalog</a></li>
+      <li><a href="page11.php">Family</a></li>
+      <li><a href="page12.php">Superhero</a></li>
+</ul>
+</h2>
+</div>
+</div>
+
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 10;
+  overflow: hidden;
+  background-color: hotpink;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 21px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: lightpink;
+}
+</style>
+
+<div>
+  <img src="adventure/adv1.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> Journey 2</a>
+ 
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;">Ever since his adventure in the center of the Earth, Sean Anderson has been looking for other places mentioned by Verne and other Vernians. He is notified about a coded message, and breaks into a satellite facility to fully receive it.</p>
+    </div>
+
+<div>
+  <img src="adventure/adventure2.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> Raiders of the Lost Ark</a>
+  
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;">The year is 1936. An archeology professor named Indiana Jones is venturing in the jungles of South America searching for a golden statue. Unfortunately, he sets off a deadly trap but miraculously escapes. Then, Jones hears from a museum curator named Marcus Brody about a biblical artifact called The Ark of the Covenant, which can hold the key to human existence.   </p>
+    </div>
+
+<div>
+  <img src="adventure/adventure3.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> The Goonies</a>
+ 
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;"> Ever since his adventure in the center of the Earth, Sean Anderson has been looking for other places mentioned by Verne and other Vernians. He is notified about a coded message, and breaks into a satellite facility to fully receive it.</p>
+    </div>
+
+<div>
+  <img src="adventure/adventure4.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> Willow</a>
+
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;"> A baby girl is discovered in a river by Ranon and Mims, the children of Willow Ufgood, a dwarf farmer and magician and the baby girl is taken into the care of Willow's family. But when a terrifying dog-like creature attacks Willow's village, whilst tracking down the baby. Willow consults the village council and the wizard The High Aldwin</p>
+    </div>
+
+<div>
+  <img src="adventure/adventure5.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";>The Never Ending Story </a>
+  
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;"> Bastian is a young boy who lives a dreary life being tormented by school bullies. On one such occasion he escapes into a book shop where the old proprieter reveals an ancient story-book to him, which he is warned can be dangerous. Shortly after, he "borrows" the book and begins to read it in the school attic where he is drawn into the mythical land of Fantasia, which desperately needs a hero to save it from destruction.</p>
+    </div>
+
+
+
+</body>
+</html>
