047cef71d76d62668a811c65884a3e9dcc2f2c46
ArevaloJoshua <joshua.arevalo435@gmail.com>
2022-05-27 21:16:04 +0800

    page 9 added

---
+++ page9.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>MovieZoneWeb</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+  
+
+  <section class="movie">
+      <nav>
+        <div>
+          <b>
+          <span style="color: white; font-size: 60px;">Movie</span>
+          <span style="color: hotpink; font-size: 60px;">Zone</span>
+        </b>
+        </div>
+
+        <div>
+          <input type="text" placeholder="Searching..." style="color: white; width: 60%; height: 50px; padding: 0px;  font-size: 20px; background-color: transparent;">
+        </div>
+      </nav>
+    </section>
+
+<style>.profile-head {
+    transform: translateY(5rem)
+}
+body {
+    background: black;
+    min-height: 100vh;
+    overflow-x: hidden
+</style>
+
+<div class="py-4 px-4">
+<div class="topnav">
+  <h2>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="page1.php">Recommended</a></li>
+      <li><a href="page2.php">Action</a></li>
+      <li><a href="page3.php">Horror</a></li>
+      <li><a href="page4.php">Romance</a></li>
+      <li><a href="page5.php">Fantasy</a></li>
+      <li><a href="page6.php">Comedy</a></li>
+      <li><a href="page7.php">K-Drama</a></li>
+      <li><a href="page8.php">Adventure</a></li>
+      <li><a href="page9.php" style="background-color:black; color: hotpink;">Animation</a></li>
+      <li><a href="page10.php">Tagalog</a></li>
+      <li><a href="page11.php">Family</a></li>
+      <li><a href="page12.php">Superhero</a></li>
+</ul>
+</h2>
+</div>
+</div>
+
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 10;
+  overflow: hidden;
+  background-color: hotpink;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 21px;
+  text-decoration: none;
+  
+}
+
+li a:hover {
+  background-color: lightpink;
+}
+</style>
+
+<div>
+  <img src="animation/ani1.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> Turning Red</a>
+ 
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;">Mei has a vivid nightmare involving red pandas. When she wakes up that morning, she has transformed into a large red panda. She hides from her parents and discovers that she transforms only when she is in a state of high emotion, though her hair remains red. Ming initially believes Mei is experiencing her first period, but learns the truth when she further humiliates Mei at school, causing her to transform and run home. Ming and Mei's father Jin explain that Sun Yee was granted this transformation to protect her daughters, and every female family member since then has also transformed when they came of age. This has become inconvenient and dangerous in the modern age, so the red panda spirit must be sealed in a talisman by a ritual on the night of the Red Moon; the next being in a month's time. Mei's friends discover her transformation but take a liking to it, and Mei finds that concentrating on them helps control the red panda within her.</p>
+
+    </div>
+
+<div>
+  <img src="animation/ani2.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";>Arabian Knight </a>
+ 
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;"> Three gold orbs must shine from a minaret to keep ancient Baghdad safe from an evil warrior and his hordes</p>
+    </div>
+
+<div>
+  <img src="animation/ani3.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> Monster House</a>
+ 
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;"> Monster House welcoms kids and adults alike into a household full of smart, monstrous fun.</p>
+    </div>
+
+<div>
+  <img src="animation/ani4.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";>Anastasia </a>
+
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;">Beautiful animation, an affable take on Russian history, and strong voice performances make Anastasia a winning first film from Fox animation studios. </p>
+    </div>
+
+<div>
+  <img src="animation/ani5.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> Paranorman</a>
+  
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;"> Beautifully animated and solidly scripted, ParaNorman will entertain (and frighten) older children while providing surprisingly thoughtful fare for their parents.</p>
+    </div>
+
+
+
+</body>
+</html>
