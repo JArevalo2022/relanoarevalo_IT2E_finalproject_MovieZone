815f162f6320895c420592380fcfb318cf2179a1
ArevaloJoshua <joshua.arevalo435@gmail.com>
2022-05-27 21:17:22 +0800

    page 11 added

---
+++ page11.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>MovieZoneWeb</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+  
+
+  <section class="movie">
+      <nav>
+        <div>
+          <b>
+         <span style="color: white; font-size: 60px;">Movie</span>
+          <span style="color: hotpink; font-size: 60px;">Zone</span>
+        </b>
+        </div>
+
+        <div>
+          <input type="text" placeholder="Searching..." style="color: white; width: 60%; height: 50px; padding: 0px;  font-size: 20px; background-color: transparent;">
+        </div>
+      </nav>
+    </section>
+
+<style>.profile-head {
+    transform: translateY(5rem)
+}
+body {
+    background: black;
+    min-height: 100vh;
+    overflow-x: hidden
+</style>
+
+<div class="py-4 px-4">
+<div class="topnav">
+  <h2>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="page1.php">Recommended</a></li>
+      <li><a href="page2.php">Action</a></li>
+      <li><a href="page3.php">Horror</a></li>
+      <li><a href="page4.php">Romance</a></li>
+      <li><a href="page5.php">Fantasy</a></li>
+      <li><a href="page6.php">Comedy</a></li>
+      <li><a href="page7.php">K-Drama</a></li>
+      <li><a href="page8.php">Adventure</a></li>
+      <li><a href="page9.php">Animation</a></li>
+      <li><a href="page10.php">Tagalog</a></li>
+      <li><a href="page11.php" style="background-color:black; color: hotpink;">Family</a></li>
+      <li><a href="page12.php">Superhero</a></li>
+</ul>
+</h2>
+</div>
+</div>
+
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 10;
+  overflow: hidden;
+  background-color: hotpink;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 21px;
+  text-decoration: none;
+
+}
+
+li a:hover {
+  background-color: lightpink;
+}
+</style>
+
+<div>
+  <img src="family/fam1.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> Chip 'N Dale</a>
+
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;"> Thirty years after their popular television show ended, chipmunks Chip and Dale live very different lives. When a cast member from the original series mysteriously disappears, the pair must reunite to save their friend.</p>
+    </div>
+
+<div>
+  <img src="family/fam2.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> The Bad Guy</a>
+
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;">Several reformed yet misunderstood criminal animals attempt to become good, with some disastrous results along the way. </p>
+    </div>
+
+<div>
+  <img src="family/fam3.jpg" style="width: 15%; height: 400px; padding-left: 10px; ">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";>Sonic The Hedgehog 2 </a>
+
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;"> When the manic Dr Robotnik returns to Earth with a new ally, Knuckles the Echidna, Sonic and his new friend Tails is all that stands in their way.</p>
+    </div>
+
+<div>
+  <img src="family/fam4.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";>Sing 2 </a>
+ 
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;"> Buster Moon and his friends must persuade reclusive rock star Clay Calloway to join them for the opening of a new show.</p>
+    </div>
+
+<div>
+  <img src="family/fam5.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> Encanto</a>
+
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;">A Colombian teenage girl has to face the frustration of being the only member of her family without magical powers. </p>
+    </div>
+
+
+
+</body>
+</html>
