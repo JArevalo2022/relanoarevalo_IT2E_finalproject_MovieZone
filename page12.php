93825e79d95466c01d0862a83d902b5cb75f0553
ArevaloJoshua <joshua.arevalo435@gmail.com>
2022-05-27 21:17:48 +0800

    page 12 added

---
+++ page12.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>MovieZoneWeb</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+  
+
+  <section class="movie">
+      <nav>
+        <div>
+          <b>
+         <span style="color: white; font-size: 60px;">Movie</span>
+          <span style="color: hotpink; font-size: 60px;">Zone</span>
+        </b>
+        </div>
+
+        <div>
+          <input type="text" placeholder="Searching..." style="color: white; width: 60%; height: 50px; padding: 0px;  font-size: 20px; background-color: transparent;">
+        </div>
+      </nav>
+    </section>
+
+<style>.profile-head {
+    transform: translateY(5rem)
+}
+body {
+    background: black;
+    min-height: 100vh;
+    overflow-x: hidden
+</style>
+
+<div class="py-4 px-4">
+<div class="topnav">
+  <h2>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="page1.php">Recommended</a></li>
+      <li><a href="page2.php">Action</a></li>
+      <li><a href="page3.php">Horror</a></li>
+      <li><a href="page4.php">Romance</a></li>
+      <li><a href="page5.php">Fantasy</a></li>
+      <li><a href="page6.php">Comedy</a></li>
+      <li><a href="page7.php">K-Drama</a></li>
+      <li><a href="page8.php">Adventure</a></li>
+      <li><a href="page9.php">Animation</a></li>
+      <li><a href="page10.php">Tagalog</a></li>
+      <li><a href="page11.php">Family</a></li>
+      <li><a href="page12.php"style="background-color:black; color: hotpink;">Superhero</a></li>
+</ul>
+</h2>
+</div>
+</div>
+
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 10;
+  overflow: hidden;
+  background-color: hotpink;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 21px;
+  text-decoration: none;
+  
+}
+
+li a:hover {
+  background-color: lightpink;
+}
+</style>
+
+<div>
+  <img src="superhero/sup1.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";>The Dark Knight </a>
+
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;"> When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.</p>
+    </div>
+
+<div>
+  <img src="superhero/sup2.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> Avengers: Endgame</a>
+
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;"> After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe.</p>
+    </div>
+
+<div>
+  <img src="superhero/sup3.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> Spider-Man: No Way Home</a>
+
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;">With Spider-Man's identity now revealed, Peter asks Doctor Strange for help. When a spell goes wrong, dangerous foes from other worlds start to appear, forcing Peter to discover what it truly means to be Spider-Man. </p>
+    </div>
+
+<div>
+  <img src="superhero/sup4.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> Logan</a>
+
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;"> In a future where mutants are nearly extinct, an elderly and weary Logan leads a quiet life. But when Laura, a mutant child pursued by scientists, comes to him for help, he must get her to safety.</p>
+    </div>
+
+<div>
+  <img src="superhero/sup5.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> Iron Man</a>
+
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;">After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil. </p>
+    </div>
+
+
+
+</body>
+</html>
