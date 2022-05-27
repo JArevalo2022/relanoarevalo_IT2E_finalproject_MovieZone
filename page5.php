67bf9ad33c7c60ddd310dfdd225be40b8ab5f36e
ArevaloJoshua <joshua.arevalo435@gmail.com>
2022-05-27 21:13:13 +0800

    page 5 added

---
+++ page5.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>MovieZoneWeb</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+  
+
+  <section class="movie">
+      <nav>
+        <div>
+          <b>
+         <span style="color: white; font-size: 60px;">Movie</span>
+          <span style="color: hotpink; font-size: 60px;">Zone</span>
+        </b>
+        </div>
+
+        <div>
+          <input type="text" placeholder="Searching..." style="color: white; width: 60%; height: 50px; padding: 0px;  font-size: 20px; background-color: transparent;">
+        </div>
+      </nav>
+    </section>
+
+<style>.profile-head {
+    transform: translateY(5rem)
+}
+body {
+    background: black;
+    min-height: 100vh;
+    overflow-x: hidden
+</style>
+
+<div class="py-4 px-4">
+<div class="topnav">
+  <h2>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="page1.php">Recommended</a></li>
+      <li><a href="page2.php">Action</a></li>
+      <li><a href="page3.php">Horror</a></li>
+      <li><a href="page4.php">Romance</a></li>
+      <li><a href="page5.php" style="background-color:black; color: hotpink;">Fantasy</a></li>
+      <li><a href="page6.php">Comedy</a></li>
+      <li><a href="page7.php">K-Drama</a></li>
+      <li><a href="page8.php">Adventure</a></li>
+      <li><a href="page9.php">Animation</a></li>
+      <li><a href="page10.php">Tagalog</a></li>
+      <li><a href="page11.php">Family</a></li>
+      <li><a href="page12.php">Superhero</a></li>
+</ul>
+</h2>
+</div>
+</div>
+
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 10;
+  overflow: hidden;
+  background-color: hotpink;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 21px;
+  text-decoration: none;
+  
+}
+
+li a:hover {
+  background-color: lightpink;
+}
+</style>
+
+<div>
+  <img src="fantasy/fan1.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";>King Arthur </a>
+
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;"> The Roman Empire is stretched across many nations, including Britain. In their conquest for more land, the Romans went into Sarmatia where they fought the very brave Sarmatian cavalry. The Romans, impressed by the Sarmatian's weaponry and fighting skills, included them into their army as knights. After 15 years of serving and fighting for the Roman Empire the Sarmatian Knights, lead by Arthur/Artorious Castus, are about to receive their freedom as the Romans are leaving Britain.</p>
+    </div>
+
+<div>
+  <img src="fantasy/fan2.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> How to Train Your Dragon 2</a>
+  
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;"> When Hiccup and Toothless discover an ice cave that is home to hundreds of new wild dragons and the mysterious Dragon Rider, the two friends find themselves at the center of a battle to protect the peace.</p>
+    </div>
+
+<div>
+  <img src="fantasy/fan3.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> Errementari</a>
+
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;"> It’s a bit of treat when one stumbles upon something they were not expecting. I was watching something on Netflix when all of a sudden, I got some random selection that said if I liked this film then try this one, which ended up being Errementari: The Blacksmith and the Devil. </p>
+    </div>
+
+<div>
+  <img src="fantasy/fan4.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";> The Fall</a>
+  
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;"> A seemingly cold but very passionate policewoman goes head to head with a seemingly passionate father who is in fact a cold serial killer in this procedural out of Belfast. The only thing they share is their common complexity.</p>
+    </div>
+
+<div>
+  <img src="fantasy/fan5.jpg" style="width: 15%; height: 400px; padding-left: 10px;">
+  <a href="url" style="color: hotpink; font-size: 50px; float: top; padding-left: 20px";>Solomon Kane </a>
+  
+</div>
+
+     <div>
+      <p style="color: white; font-size: 20px; float: center; padding-left: 20px; margin-right: 950px;">A ruthless mercenary renounces violence after learning his soul is bound for hell. When a young girl is kidnapped and her family slain by a sorcerer's murderous cult, he is forced to fight and seek his redemption slaying evil. </p>
+    </div>
+
+
+
+</body>
+</html>
